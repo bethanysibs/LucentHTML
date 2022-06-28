30bf49330b3f826e699c0635f5eb15209d42d283
bethanysibs <bethanyjoycesibala@gmail.com>
2022-06-29 00:49:31 +0800

    Revert "updated css"
    This reverts commit 15bd653a9acd19cba10aef31ca00b970211b9786.

--- users.php
+++ users.php
@@ -1,7 +1,18 @@
 <!DOCTYPE html>
 <html lang="en" class="dark">
-       <!-- BEGIN: Head -->
-<?php include 'header.php'; ?>
+    <!-- BEGIN: Head -->
+    <head>
+        <meta charset="utf-8">
+        <link href="dist/images/logo.svg" rel="shortcut icon">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Rocketman admin is super flexible, powerful, clean & modern responsive tailwind admin template with unlimited possibilities.">
+        <meta name="keywords" content="admin template, Rocketman Admin Template, dashboard template, flat admin template, responsive admin template, web app">
+        <meta name="author" content="LEFT4CODE">
+        <title>Users - Paper +</title>
+        <!-- BEGIN: CSS Assets-->
+        <link rel="stylesheet" href="dist/css/app.css" />
+        <!-- END: CSS Assets-->
+    </head>
     <!-- END: Head -->
     <body class="main">
         <div class="xl:pl-5 xl:py-5 flex h-screen">           
