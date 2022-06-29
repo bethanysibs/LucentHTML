5a04a8800176b613b67f48734cea878226582b82
bethanysibs <bethanyjoycesibala@gmail.com>
2022-06-29 18:10:08 +0800

    updates

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" class="light">
     <!-- BEGIN: Head -->
+    <title>Login - Paper +</title>
 <?php include 'header.php'; ?>
     <!-- END: Head -->
     <body class="login">
@@ -20,10 +21,10 @@
             </div>
         </div>
         <!-- BEGIN: Dark Mode Switcher-->
-        <div data-url="login-light2.php" class="dark-mode-switcher cursor-pointer shadow-md fixed bottom-0 right-0 box border rounded-full w-40 h-12 flex items-center justify-center z-50 mb-10 mr-10">
+        <!-- <div data-url="login-light2.php" class="dark-mode-switcher cursor-pointer shadow-md fixed bottom-0 right-0 box border rounded-full w-40 h-12 flex items-center justify-center z-50 mb-10 mr-10">
             <div class="mr-4 text-slate-600 dark:text-slate-200">Dark Mode</div>
             <div class="dark-mode-switcher__toggle border"></div>
-        </div>
+        </div> -->
         <!-- END: Dark Mode Switcher-->
         
         <!-- BEGIN: JS Assets-->
